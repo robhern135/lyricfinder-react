a9121111801da274c16dc1ab1460127a3e689b67
robhern135 <robertjhern@gmail.com>
2021-01-30 17:06:29 +0000

    Updates

---
+++ precache-manifest.19e6e5dcaf72f3519f3827a26398ea50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000fc59bc26897ec664e2ea1b71cc6ff",
+    "url": "/lyricfinder-react/index.html"
+  },
+  {
+    "revision": "69065eb4a791624812e3",
+    "url": "/lyricfinder-react/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c3bdde22e434e39f6f2d",
+    "url": "/lyricfinder-react/static/js/2.278991bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lyricfinder-react/static/js/2.278991bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69065eb4a791624812e3",
+    "url": "/lyricfinder-react/static/js/main.22b76602.chunk.js"
+  },
+  {
+    "revision": "1fa929369a727d00f3ac",
+    "url": "/lyricfinder-react/static/js/runtime-main.2d102d27.js"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/lyricfinder-react/static/media/spinner.235b9b3e.gif"
+  }
+]);
