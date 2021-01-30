fe8713810d51ccdcd7b0f2dd1dd55d1a97f67ffe
robhern135 <robertjhern@gmail.com>
2021-01-30 17:04:03 +0000

    publishing to gh pages

---
+++ src/components/tracks/Track.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Track = (props) => {
+  const { track } = props;
+  return (
+    <div className="col-md-6">
+      <div className="card mb-4 shadow-sm">
+        <div className="card-body">
+          <h5>{track.artist_name}</h5>
+          <p className="card-text">
+            <strong>
+              <i className="fas fa-music"></i> Track
+            </strong>
+            : {track.track_name}
+          </p>
+          <p className="card-text">
+            <strong>
+              <i className="fas fa-compact-disc"></i> Album
+            </strong>
+            : {track.album_name}
+          </p>
+          <Link
+            className="btn btn-dark btn-block"
+            to={`lyrics/track/${track.track_id}`}
+          >
+            <i className="fas-fa-chevron-right"></i> View Lyrics
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Track;
